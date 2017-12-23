a0e2c75694e9031df88a428c64a9d089dd2d1ecd
Charlotte Fereday <cfereday@thoughtworks.com>
2017-12-23 19:19:58 +0100

    [Charlotte] refactors but still error message

--- leap.js
+++ leap.js
@@ -1,13 +1,13 @@
 var Year = function (input) {
     Year.prototype.isLeap = function (input) {
         let trueOrFalse = null;
-        if (this.isEven(input, 4) === true) {
+        if (this.isEven(input, 4) === true && this.isEven(input, 100) === false) {
+            trueOrFalse = true; }
+        else if (this.isEven(input, 4) === true && this.isEven(input, 100) === true) {
             if (this.isEven(input, 100) === true && this.isEven(input, 400) === true) {
                 trueOrFalse = true;
             } else if (this.isEven(input, 100) === true && this.isEven(input, 400) === false) {
-                trueOrFalse = false;
-            } else if (this.isEven(input, 100) === false) {
-                trueOrFalse = true;
+                    trueOrFalse = false;
             }
         } else {
             trueOrFalse = false;
@@ -15,9 +15,9 @@ var Year = function (input) {
         return trueOrFalse;
     };
 
-    Year.prototype.isEven = function (input, integer)  {
-      let oddOrEven = input / integer;
-      return oddOrEven % 2 === 0 ? true : false;
+    Year.prototype.isEven = function (input, integer) {
+        let oddOrEven = input / integer;
+        return oddOrEven % 2 === 0 ? true : false;
     };
 };
 
