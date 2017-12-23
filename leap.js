f39ec5996877c8a0bbd7233c053d13a92cee52a4
Charlotte Fereday <cfereday@thoughtworks.com>
2017-12-23 19:10:57 +0100

    [Charlotte] further refactor however WIP as one test currently failing

---
+++ leap.js
@@ -0,0 +1,24 @@
+var Year = function (input) {
+    Year.prototype.isLeap = function (input) {
+        let trueOrFalse = null;
+        if (this.isEven(input, 4) === true) {
+            if (this.isEven(input, 100) === true && this.isEven(input, 400) === true) {
+                trueOrFalse = true;
+            } else if (this.isEven(input, 100) === true && this.isEven(input, 400) === false) {
+                trueOrFalse = false;
+            } else if (this.isEven(input, 100) === false) {
+                trueOrFalse = true;
+            }
+        } else {
+            trueOrFalse = false;
+        }
+        return trueOrFalse;
+    };
+
+    Year.prototype.isEven = function (input, integer)  {
+      let oddOrEven = input / integer;
+      return oddOrEven % 2 === 0 ? true : false;
+    };
+};
+
+module.exports = Year;
