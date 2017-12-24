189d925d4874377d3377af4a6544bf18857f9f69
Charlotte Fereday <cfereday@thoughtworks.com>
2017-12-24 23:39:32 +0000

    [Charlotte] passes tests

--- leap.js
+++ leap.js
@@ -1,24 +1,35 @@
 var Year = function (input) {
     Year.prototype.isLeap = function (input) {
         let trueOrFalse = null;
-        if (this.isEven(input, 4) === true && this.isEven(input, 100) === false) {
-            trueOrFalse = true; }
-        else if (this.isEven(input, 4) === true && this.isEven(input, 100) === true) {
-            if (this.isEven(input, 100) === true && this.isEven(input, 400) === true) {
-                trueOrFalse = true;
-            } else if (this.isEven(input, 100) === true && this.isEven(input, 400) === false) {
-                    trueOrFalse = false;
-            }
+        if (this.isDivisblebyFour(input) === true && this.isDivisbleby100(input) === false ) {
+            trueOrFalse = true;
+        } else if (this.isDivisblebyFour(input) === true && this.isDivisbleby100(input) === true && this.isDivisbleby400(input) === true ){
+            trueOrFalse = true;
         } else {
             trueOrFalse = false;
         }
         return trueOrFalse;
     };
 
-    Year.prototype.isEven = function (input, integer) {
-        let oddOrEven = input / integer;
-        return oddOrEven % 2 === 0 ? true : false;
+    Year.prototype.isDivisblebyFour = function (input) {
+        return input % 4 === 0 ? true : false;
+    };
+
+    Year.prototype.isDivisbleby100 = function (input) {
+        return input % 100 === 0 ? true : false;
+    };
+
+    Year.prototype.isDivisbleby400 = function (input) {
+        return input % 400 === 0 ? true : false;
     };
 };
 
 module.exports = Year;
+
+// every 4 years there is a leap year - if divisible by 4 will be unless it's also divisible by 100
+// every century is skipped - if divisible by 100 won't be a leap year
+// if divisible by 400 will be  a leap year
+//
+// if it's a multiple of 4
+//     skip if a new century
+// unless divisible by 400
